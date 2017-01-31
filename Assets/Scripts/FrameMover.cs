c23a62712fe9abad6603e04409c07e4788b9c475
Chandler <ttc29@n1-140-37.dhcp.drexel.edu>
2017-01-31 14:52:51 -0500

    Added in main game scene. /n Added in basic scripts. /n Added in floating platform.

---
+++ Assets/Scripts/FrameMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameMover : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+	public Vector2 DeleteVector;
+	public Vector2 SpawnVector;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public Vector2 StartVector, EndVector;
+	public static GameManager instance;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (this);
+			return;
+		}
+		//Do things with the instance here;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
