b3e96660d6d0c291e0df548d578f7429d5b85560
Travis Chandler <travestychandler@gmail.com>
2017-02-07 11:35:56 -0500

    Implemented frame movement and spawning.

--- Assets/Scripts/FrameMover.cs
+++ Assets/Scripts/FrameMover.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class FrameMover : MonoBehaviour {
 
-	public float moveSpeed = 1f;
-	public Vector2 DeleteVector;
-	public Vector2 SpawnVector;
+    public Transform start, end;
 	// Use this for initialization
 	void Start () {
-	
+      
 	}
-	
+
+    public void FramePlacement(FrameMover other)
+    {
+        Transform otherEnd = other.end;
+        float dist = this.transform.position.x - this.start.position.x;
+        this.transform.position = new Vector3(otherEnd.position.x + dist, 0);
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        if (transform.position.x < GameManager.instance.EndVector.x)
+        {
+            Despawn();
+        }
+        else
+        {
+            this.transform.Translate(new Vector2(GameManager.instance.CurrentSpeed, 0f) * Time.deltaTime);
+        }
 	}
+
+    public void Despawn()
+    {
+        GameManager.instance.Despawn(this);
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 	public Vector2 StartVector, EndVector;
+    public float CurrentSpeed = 1f;
 	public static GameManager instance;
-
+    public List<GameObject> FramePrefabs;
+    public List<FrameMover> frames;
+    public int startFrames = 3;
 	// Use this for initialization
 	void Start () {
 		if (instance == null) {
@@ -13,11 +17,39 @@ public class GameManager : MonoBehaviour {
 			Destroy (this);
 			return;
 		}
-		//Do things with the instance here;
-	}
-	
+        //Do things with the instance here;
+        StartGame();
+
+    }
+
+    public void StartGame()
+    {
+        for(int i =0;  i < startFrames; i++)
+        {
+            Spawn();
+        }
+    }
+
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void Despawn(FrameMover frame)
+    {
+        Spawn();
+        frames.Remove(frame);
+        Destroy(frame.gameObject);
+    }
+
+    public void Spawn()
+    {
+        int rand = Random.Range(0, FramePrefabs.Count);
+        GameObject frameObj = Instantiate(FramePrefabs[rand], Vector3.one * 100f, Quaternion.identity);
+        FrameMover frame = frameObj.GetComponent<FrameMover>();
+        frames.Add(frame);
+        frame.FramePlacement(frames[frames.Count - 2]);
+
+    }
 }
