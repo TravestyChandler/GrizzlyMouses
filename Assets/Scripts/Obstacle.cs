e22cbf46b9b7c9ff9a5b7509825f50a77a72fbd5
Chandler,Travis <ttc29@drexel.edu>
2017-04-18 13:50:08 -0400

    Implemented obstacle collisions for the Easy4 frame

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
 	public GameObject particlePrefab;
+	public PhotonView phot;
 	// Use this for initialization
 	void Start () {
-		
+		phot = this.GetComponent<PhotonView> ();
+		phot.viewID = PhotonNetwork.AllocateViewID ();
 	}
 	
 	// Update is called once per frame
@@ -14,13 +16,24 @@ public class Obstacle : MonoBehaviour {
 		
 	}
 
+	public bool destroying = false;
+
+	public void OnCollisionEnter2D(Collision2D col){
+		if (!destroying) {
+			if (col.gameObject.tag.Equals ("Player")) {
+				phot.RPC ("DestroyObstacle", PhotonTargets.All, null);
+				destroying = true;
+			}
+		}
+	}
 	[PunRPC]
 	public void DestroyObstacle(){
 		foreach(Collider2D col in this.GetComponents<Collider2D>()){
 			col.enabled = false;
 		}
 		GameObject game = GameObject.Instantiate (particlePrefab, this.transform.position, Quaternion.identity);
+		Destroy (this.gameObject, 0.1f);
 		Destroy (game,3f);
-		Destroy (this.gameObject, 3f);
+
 	}
 }
